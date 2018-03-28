5ce08b94b21249c5e1783bf5b70741d760ebfedc
kristieboyd <37085331+kristieboyd@users.noreply.github.com>
2018-03-28 13:38:26 -0230

    1

---
+++ app/src/main/java/com/example/picmap/LoginActivity.java
@@ -0,0 +1,139 @@
+package com.example.picmap;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private LinearLayout Prof_Section;
+    private Button MapButton;
+    private Button SignOut;
+    private SignInButton SignIn;
+    private TextView Name, Email;
+    private ImageView Prof_Pic;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Prof_Section = (LinearLayout) findViewById(R.id.prof_section);
+        MapButton = (Button) findViewById(R.id.bn_map);
+        SignOut = (Button) findViewById(R.id.bn_logout);
+        SignIn = (SignInButton) findViewById(R.id.bn_login);
+        Name = (TextView) findViewById(R.id.name);
+        Email = (TextView) findViewById(R.id.email);
+        Prof_Pic = (ImageView) findViewById(R.id.prof_pic);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Prof_Section.setVisibility(View.GONE);
+        MapButton.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bn_login:
+                signIn();
+                break;
+            case R.id.bn_logout:
+                signOut();
+                break;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void signIn() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, REQ_CODE);
+
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void handleResult(GoogleSignInResult result) {
+        GoogleSignInAccount account = result.getSignInAccount();
+        if (result.isSuccess() && GoogleSignIn.hasPermissions(account, new Scope(Scopes.DRIVE_APPFOLDER))) { //if (result.isSuccess())
+            //GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            Uri img_url = account.getPhotoUrl();
+            Name.setText(name);
+            Email.setText(email);
+            Prof_Pic.setImageURI(img_url);
+            updateUI(true);
+        } else {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin) {
+        if (isLogin) {
+            Prof_Section.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+            MapButton.setVisibility(View.VISIBLE);
+        } else {
+            Prof_Section.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+            MapButton.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }
+
+    public void displayMap(View view) {
+        System.out.println("button");
+        Intent myIntent = new Intent(view.getContext(), MapsActivity.class);
+        startActivityForResult(myIntent, 0);
+
+    }
+
+}
